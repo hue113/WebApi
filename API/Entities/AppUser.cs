5aabb78e3f1f97a426121cba5f37ed94825c12e6
Hue Pham <huepham113@gmail.com>
2023-08-31 00:42:00 -0400

    part 3: authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
 
 namespace API.Entities
 {
@@ -9,5 +11,7 @@ namespace API.Entities
   {
     public int Id { get; set; }
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
   }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +15,25 @@ builder.Services.AddDbContext<DataContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+  options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+    ValidateIssuer = false,
+    ValidateAudience = false
+  };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication(); // check if user's token is valid 
+app.UseAuthorization(); // check what user can do after their token is valid
 
 app.MapControllers();
 
