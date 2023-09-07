ac59dfa5affe1bdb861c59093a67137410a8eb9c
Hue Pham <huepham113@gmail.com>
2023-09-06 23:46:54 -0400

    wip: Parent to Child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { AccountsService } from './_services/accounts.service';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 
@@ -10,30 +9,14 @@ import { User } from './_models/user';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountsService
-  ) {}
+  constructor(private accountService: AccountsService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log('err', error),
-      complete: () => console.log('request has completed'),
-    });
-  }
-
   setCurrentUser() {
-    // const user: User = JSON.parse(localStorage.getItem('user')); // type error
-    // const user: User = JSON.parse(localStorage.getItem('user')!); // use ! to remove type check
-    // best is do like this:
     const userString = localStorage.getItem('user');
     if (!userString) return;
     const user: User = JSON.parse(userString);
