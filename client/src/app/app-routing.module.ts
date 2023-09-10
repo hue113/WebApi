e6d34ec6597a6d97d678461249fb855f4f1136e6
Hue Pham <huepham113@gmail.com>
2023-09-09 21:28:07 -0400

    wip: use authGuard for selected routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,13 +10,20 @@ import { authGuard } from './_guards/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: 'members',
-    component: MemberListComponent,
-    canActivate: [authGuard], // pass any check you'd like to the array to check if users can pass the route
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'members',
+        component: MemberListComponent,
+        canActivate: [authGuard], // pass any check you'd like to the array to check if users can pass the route
+      },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
   },
-  { path: 'members/:id', component: MemberDetailComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }, // wildcard
 ];
 
