4f8141b6225fc711a70085e5faeb28fb8e3fd8ca
Hue Pham <huepham113@gmail.com>
2023-09-12 22:45:23 -0400

    Section 8-88: Entity Framework Relationship

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+  [Table("Photos")] // create table Photos & set up relationship bw AppUser-Photo
   public class Photo
   {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
-    // public int AppUserId { get; set; }
-    // public AppUser AppUser { get; set; }
+
+    // Need to add these 2 relationship properties to define the relationship between Photo-AppUser
+    // https://learn.microsoft.com/en-us/ef/core/modeling/relationships
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
   }
 }
