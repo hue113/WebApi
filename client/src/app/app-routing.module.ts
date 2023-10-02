50fd24154b7536b5e36550448f14ac5293bc87b9
Hue Pham <huepham113@gmail.com>
2023-10-02 12:03:09 -0400

    Section 10-120: Add CanDeactivate route guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,13 +24,11 @@ const routes: Routes = [
         component: MemberListComponent,
         canActivate: [authGuard], // pass any check you'd like to the array to check if users can pass the route
       },
-      {
-        path: 'members/:username',
-        component: MemberDetailComponent,
-      },
+      { path: 'members/:username', component: MemberDetailComponent },
       {
         path: 'member/edit',
         component: MemberEditComponent,
+        canDeactivate: [preventUnsavedChangesGuard],
       },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
